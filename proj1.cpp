301cc5551660eb1a090d780f1ce17d1b0b7d051e
antiphy <antiphycz@gmail.com>
2024-03-06 20:46:57 +0100

    testing

--- proj1.cpp
+++ proj1.cpp
@@ -1,5 +1,6 @@
 #include "proj1.hpp"
 
+
 void print_server(server* server_info){
     std::cout << "Protocol:" << server_info->protocol << std::endl;
     std::cout << "IP/Hostname:" << server_info->ip_hostname << std::endl;
@@ -10,7 +11,7 @@ void print_server(server* server_info){
 
 
 void argument_parsing(int argc, char* argv[], server* server_info){
-    int cli_arg;
+    int cli_arg, server_port;
 
     //Argument parsing
     while((cli_arg = getopt(argc, argv, "t:s:p:d:r:h")) != -1){
@@ -23,7 +24,12 @@ void argument_parsing(int argc, char* argv[], server* server_info){
             server_info->ip_hostname = optarg;
             break;
         case 'p':
-            server_info->port = std::stoi(optarg, nullptr, 10);
+            server_port = std::stoi(optarg, nullptr, 10);
+            if(server_port < 0 || server_port > 65535){
+                std::cerr << "ERR: Port out of range.";
+                exit(EXIT_FAILURE);
+            }
+            server_info->port = optarg;
             break;
         case 'd':
             server_info->udp_timeout = std::stoi(optarg, nullptr, 10);
@@ -45,6 +51,21 @@ int main(int argc, char* argv[]){
     server* server_info = new server;
     argument_parsing(argc, argv, server_info);
     print_server(server_info);
+
+    struct addrinfo hints = {0};
+    struct addrinfo* results;
+    //std::memset(&hints, 0, sizeof(hints));
+    //std::cout << hints.ai_addr << std::endl;
+    hints.ai_family = AF_INET;
+    hints.ai_socktype = SOCK_DGRAM;
+
+    int retreived_info;
+    if ((retreived_info = getaddrinfo(server_info->ip_hostname.c_str(), server_info->port.c_str(), &hints, &results)) != 0){
+        std::cout << "ERR: Could not resolve hostname." << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    std::cout << server_info->ip_hostname.c_str() << std::endl;
     std::cout << "END OF PROGRAM.";
     return 0;
 }
--- proj1.hpp
+++ proj1.hpp
@@ -1,13 +1,31 @@
 #include <iostream>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netdb.h>
+#include <arpa/inet.h>
+#include <netinet/in.h>
 #include <unistd.h>
+#include <limits.h>
+#include <stdbool.h>
 #include <string>
+#include <cstring>
 
 typedef struct INFO{
     std::string protocol;
     std::string ip_hostname;
-    uint16_t port;
+    std::string port;
     uint16_t udp_timeout;
     uint16_t max_udp_retransmission;
 } server;
+
+typedef enum {
+    AUTH,
+    JOIN,
+    ERR,
+    BYE,
+    MSG,
+    REPLY,
+    NOT_REPLY,
+} states;
 
 void argument_parsing(int argc, char* argv[], server* server_info);
