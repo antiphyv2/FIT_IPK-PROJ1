e8b5a9c1b32b286d123de34cebe88a4e25ec58d7
antiphy <antiphycz@gmail.com>
2024-03-30 09:49:18 +0100

    comments funtions

--- clients.hpp
+++ clients.hpp
@@ -21,13 +21,59 @@ class NetworkClient{
         
         NetworkClient(connection_info* info);
     public:
+        /**
+         * @brief Virtual method to receive message in buffer
+         * 
+         * @param msg containing buffer in which incoming mesage will be stored
+         * @return size_t bytes received
+         */
         virtual size_t accept_msg(NetworkMessage& msg) = 0;
+
+        /**
+         * @brief Sends message over socket networking
+         * 
+         * @param msg to be send over socket
+         */
         void send_msg(NetworkMessage& msg);
+
+        /**
+         * @brief Returns pointer to structure with CLI provided arguments
+         * 
+         * @return connection_info* pointer to the structure
+         */
         connection_info* get_arg_info();
+
+        /**
+         * @brief Gets pointer to the network socket 
+         * 
+         * @return ClientSocket* pointer to the socket
+         */
         ClientSocket* get_socket();
+
+        /**
+         * @brief Returns pointer to structure with client information
+         * 
+         * @return client_info* pointer to the structure
+         */
         client_info* get_cl_info();
+
+        /**
+         * @brief Translates domain name to IP address
+         * 
+         */
         void dns_lookup();
+
+        /**
+         * @brief Connects to server with parameters in dns structure
+         * 
+         */
         void establish_connection();
+
+        /**
+         * @brief Returns pointer to structure with dns information
+         * 
+         * @return struct addrinfo* pointer to the structure
+         */
         struct addrinfo* get_dns_info();
         virtual ~NetworkClient();
     
